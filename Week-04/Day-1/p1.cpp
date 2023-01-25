d1228caf02dd182c0d7bc34227201121ab4fef51
Yumna Waris <yumnaahwaris@gmail.com>
2023-01-25 07:40:15 +0500

    Character Array Basics

---
+++ Week-04/Day-1/p1.cpp
@@ -0,0 +1,30 @@
+// Palindrome Check
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    char arr[n + 1];
+    cin >> arr;
+
+    bool flag = 1;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] != arr[n - 1 - i])
+        {
+            flag = 0;
+            break;
+        }
+    }
+    if (flag == true)
+    {
+        cout << "is Palindome." << endl;
+    }
+    else
+    {
+        cout << "not a Palindrome." << endl;
+    }
+    return 0;
+}
---
+++ Week-04/Day-1/p2.cpp
@@ -0,0 +1,10 @@
+// Largest word in a sentence
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    return 0;
+}
