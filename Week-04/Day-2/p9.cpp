f95636b35dbebcf2a02c1037f6406c91141ed779
Yumna Waris <yumnaahwaris@gmail.com>
2023-01-29 17:44:34 +0500

    .

--- Week-04/Day-2/p9.cpp
+++ Week-04/Day-2/p9.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
 {
-    string phone = "021 3356910909";
+    string phone = "021 3344556677";
 
     cout << phone.erase(0, 4).insert(0, "0") << endl;
     return 0;
